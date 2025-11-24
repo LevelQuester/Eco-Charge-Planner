84f764fcead37a886e484c8cad09b39ee675bbd7
Raman Hulis <hulis.raman@gmail.com>
2025-11-24 13:01:53 +0100

    fix: adding prefix to api

--- api/src/main/java/com/codibly/energymix/controllers/EnergyController.java
+++ api/src/main/java/com/codibly/energymix/controllers/EnergyController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -14,6 +15,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class EnergyController {
 
